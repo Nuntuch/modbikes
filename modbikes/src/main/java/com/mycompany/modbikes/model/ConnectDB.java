ea3d86a34bd2d5856ab6d6902f65be0cb1d3e9d5
Nuntuch <nuntuch13589@gmail.com>
2021-04-14 11:32:38 +0700

    Format Code และ ลบ Comment ที่ไม่ได้ใช้ทิ้ง

--- modbikes/src/main/java/com/mycompany/modbikes/model/ConnectDB.java
+++ modbikes/src/main/java/com/mycompany/modbikes/model/ConnectDB.java
@@ -16,16 +16,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import sun.security.util.Password;
 
-/**
- *
- * @author postgresqltutorial.com
- */
 public class ConnectDB {
 
-//    jdbc:postgresql://<host>:<port>/<dbname>?user=<username>&password=<password>
-//    private final String url = "jdbc:postgresql://ec2-3-234-85-177.compute-1.amazonaws.com:5432/der31l0fqg9f6b?user=nkqwwjhlpbldun&password=fd0a974ab729bef38965fe05a1dd9132d0b07dba3e34d598a540120c2cd84231";
-//    private final String user = "nkqwwjhlpbldun";
-//    private final String password = "fd0a974ab729bef38965fe05a1dd9132d0b07dba3e34d598a540120c2cd84231";
     public static void main(String args[]) {
 //             url = jdbc:postgresql://<host>:<port>/<dbname>?user=<username>&password=<password>
         String url = "jdbc:postgresql://ec2-3-234-85-177.compute-1.amazonaws.com:5432/der31l0fqg9f6b?user=nkqwwjhlpbldun&password=fd0a974ab729bef38965fe05a1dd9132d0b07dba3e34d598a540120c2cd84231";
@@ -44,57 +36,14 @@ public class ConnectDB {
             System.exit(0);
         }
         System.out.println("Opened database successfully");
-        
-        
+
         try {
             c.close();
         } catch (SQLException ex) {
             Logger.getLogger(ConnectDB.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         System.out.println("Close database successfully");
-        
+
     }
 }
-
-//    /**
-//     * Connect to the PostgreSQL database
-//     *
-//     * @return a Connection object
-//     */
-//    public Connection connect() {
-//        Connection conn = null;
-//        try {
-//            conn = DriverManager.getConnection(url, user, password);
-//            System.out.println("Connected to the PostgreSQL server successfully.");
-//        } catch (SQLException e) {
-//
-//            System.out.println(e.getMessage());
-//        }
-//
-//        try {
-//            conn.close();
-//            System.out.println("Close Connected to the PostgreSQL server successfully.");
-//        } catch (SQLException ex) {
-//            Logger.getLogger(ConnectDB.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//
-//        return conn;
-//    }
-//
-//    /**
-//     * @param args the command line arguments
-//     */
-//    public static void main(String[] args) {
-//        ConnectDB app = new ConnectDB();
-////        Connection connection = app.connect();
-////        
-////        if(connection != null){
-////            System.out.println("");
-////        
-////        }
-//
-//        app.connect();
-//
-//    }
-//}
--- modbikes/src/main/java/com/mycompany/modbikes/model/Insert.java
+++ modbikes/src/main/java/com/mycompany/modbikes/model/Insert.java
@@ -17,62 +17,58 @@ import java.util.logging.Logger;
  * @author Admin
  */
 public class Insert {
-public static void main(String args[]) {
-    
+
+    public static void main(String args[]) {
+
 //             url = jdbc:postgresql://<host>:<port>/<dbname>?user=<username>&password=<password>
         String url = "jdbc:postgresql://ec2-3-234-85-177.compute-1.amazonaws.com:5432/der31l0fqg9f6b?user=nkqwwjhlpbldun&password=fd0a974ab729bef38965fe05a1dd9132d0b07dba3e34d598a540120c2cd84231";
         String user = "nkqwwjhlpbldun";
         String password = "fd0a974ab729bef38965fe05a1dd9132d0b07dba3e34d598a540120c2cd84231";
 
-    
-      Connection c = null;
-      Statement stmt = null;
-      try {
-         Class.forName("org.postgresql.Driver");
-         c = DriverManager
-            .getConnection(url,
-            user, password);
-         c.setAutoCommit(false);
-         System.out.println("Opened database successfully");
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection(url,
+                            user, password);
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
 
-         stmt = c.createStatement();
-         String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
-            + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
-         stmt.executeUpdate(sql);
+            stmt = c.createStatement();
+            String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (1, 'Paul', 32, 'California', 20000.00 );";
+            stmt.executeUpdate(sql);
 
-         sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
-            + "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
-         stmt.executeUpdate(sql);
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );";
+            stmt.executeUpdate(sql);
 
-         sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
-            + "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
-         stmt.executeUpdate(sql);
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );";
+            stmt.executeUpdate(sql);
 
-         sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
-            + "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
-         stmt.executeUpdate(sql);
+            sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "
+                    + "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
+            stmt.executeUpdate(sql);
 
-         stmt.close();
-         c.commit();
-         c.close();
-      } catch (Exception e) {
-         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
-         System.exit(0);
-      }
-      System.out.println("Records created successfully");
-
-
+            stmt.close();
+            c.commit();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
 
         try {
             c.close();
         } catch (SQLException ex) {
             Logger.getLogger(ConnectDB.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         System.out.println("Close database successfully");
 
-   }
-
-
+    }
 
 }
