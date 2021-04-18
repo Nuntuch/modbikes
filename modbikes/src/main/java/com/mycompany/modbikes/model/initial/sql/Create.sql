4a2d947a5248548c83d03576db15d8cad8d74227
Nuntuch <nuntuch13589@gmail.com>
2021-04-18 16:06:29 +0700

    สร้าง sql statement สำหรับ create table ตาม dirgram
    https://app.diagrams.net/#G1F0GvCdBytC4SKD2D7ENxjFwNn_hsTSOX

--- modbikes/src/main/java/com/mycompany/modbikes/model/initial/sql/Create.sql
+++ modbikes/src/main/java/com/mycompany/modbikes/model/initial/sql/Create.sql
@@ -8,3 +8,68 @@
  * Created: Apr 14, 2021
  */
 
+DROP TABLE IF EXISTS Location;
+DROP TABLE IF EXISTS Bicycle;
+DROP TABLE IF EXISTS History;
+DROP TABLE IF EXISTS Student;
+
+CREATE TABLE Student(
+   StudentID bigint NOT NULL,
+   FirstName VARCHAR(255) NOT NULL,
+   LastName VARCHAR(255) NOT NULL,
+   Faculty VARCHAR(255) NOT NULL,
+   Department VARCHAR(255) NOT NULL,
+   Password VARCHAR(255) NOT NULL,
+   IsUseBike boolean NOT NULL,
+   TelephoneNo VARCHAR(255) NOT NULL,
+   Email VARCHAR(255) NOT NULL,
+   OtherContact VARCHAR(255) NOT NULL,
+   PRIMARY KEY(StudentID)
+);
+
+CREATE TABLE History(
+   HistoryID INT NOT NULL,
+   StudentID bigint NOT NULL,
+   BikeID bigint NOT NULL,
+   StartDate timestamp NOT NULL,
+   FinishedDate timestamp NOT NULL,
+   LastLocation VARCHAR(255) NOT NULL,
+   IsReturnBikeOntime boolean NOT NULL,
+   PRIMARY KEY(HistoryID),
+   CONSTRAINT fk_student
+      FOREIGN KEY(StudentID) 
+	  REFERENCES Student(StudentID)
+);
+
+
+
+CREATE TABLE Location(
+   LocationID INT NOT NULL,
+   LocationName VARCHAR(255) NOT NULL,
+   LocationPicLink VARCHAR(255) NOT NULL,
+   Latitude real NOT NULL,
+   Londtitude real NOT NULL,
+   PRIMARY KEY(LocationID)
+);
+
+
+
+
+
+CREATE TABLE Bicycle(
+   BikeID INT NOT NULL,
+   LocationID INT NOT NULL,
+   IsAvailable boolean NOT NULL,
+   Detail VARCHAR(255) NOT NULL,
+   BikePicLink VARCHAR(255) NOT NULL,
+   PRIMARY KEY(BikeID),
+   CONSTRAINT fk_location
+      FOREIGN KEY(LocationID) 
+	  REFERENCES Location(LocationID)
+);
+
+
+
+
+
+
