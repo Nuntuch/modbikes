b17bd579fd6dc74898f1248a859610d01ce50b6e
Nuntuch <nuntuch13589@gmail.com>
2021-04-19 17:09:07 +0700

    สร้าง sql statement สำหรับ insert ข้อมูลของตาราง Location และ Bicycle

--- modbikes/src/main/java/com/mycompany/modbikes/model/initial/sql/Inseart.sql
+++ modbikes/src/main/java/com/mycompany/modbikes/model/initial/sql/Inseart.sql
@@ -49,7 +49,29 @@ VALUES ( 46000100008, 'Wada', 'Maaya', 'Group46', 'Nogizaka46', '12646',FALSE,'0
 
 -- __________________________Start_Location__________________________
 
+INSERT INTO Location (LocationID, LocationName , LocationPicLink, Latitude ,Londtitude )
+VALUES ( 1, 'SIT', 'https://drive.google.com/thumbnail?id=1t26xroWSf1drL9pw8crvCxGeAghTqrt2', 0.0, 0.0);
 
+INSERT INTO Location (LocationID, LocationName , LocationPicLink, Latitude ,Londtitude )
+VALUES ( 2, 'SIT', 'https://drive.google.com/thumbnail?id=1t26xroWSf1drL9pw8crvCxGeAghTqrt2', 0.0, 0.0);
+
+INSERT INTO Location (LocationID, LocationName , LocationPicLink, Latitude ,Londtitude )
+VALUES ( 3, 'SIT', 'https://drive.google.com/thumbnail?id=1t26xroWSf1drL9pw8crvCxGeAghTqrt2', 0.0, 0.0);
+
+INSERT INTO Location (LocationID, LocationName , LocationPicLink, Latitude ,Londtitude )
+VALUES ( 4, 'SIT', 'https://drive.google.com/thumbnail?id=1t26xroWSf1drL9pw8crvCxGeAghTqrt2', 0.0, 0.0);
+
+INSERT INTO Location (LocationID, LocationName , LocationPicLink, Latitude ,Londtitude )
+VALUES ( 5, 'SIT', 'https://drive.google.com/thumbnail?id=1t26xroWSf1drL9pw8crvCxGeAghTqrt2', 0.0, 0.0);
+
+INSERT INTO Location (LocationID, LocationName , LocationPicLink, Latitude ,Londtitude )
+VALUES ( 6, 'SIT', 'https://drive.google.com/thumbnail?id=1t26xroWSf1drL9pw8crvCxGeAghTqrt2', 0.0, 0.0);
+
+INSERT INTO Location (LocationID, LocationName , LocationPicLink, Latitude ,Londtitude )
+VALUES ( 7, 'SIT', 'https://drive.google.com/thumbnail?id=1t26xroWSf1drL9pw8crvCxGeAghTqrt2', 0.0, 0.0);
+
+INSERT INTO Location (LocationID, LocationName , LocationPicLink, Latitude ,Londtitude )
+VALUES ( 8, 'SIT', 'https://drive.google.com/thumbnail?id=1t26xroWSf1drL9pw8crvCxGeAghTqrt2', 0.0, 0.0);
 
 
 -- ___________________________End_Location____________________________
@@ -60,16 +82,39 @@ VALUES ( 46000100008, 'Wada', 'Maaya', 'Group46', 'Nogizaka46', '12646',FALSE,'0
 -- __________________________Start_Bicycle__________________________
 
 
+INSERT INTO Bicycle ( BikeID , LocationID ,IsAvailable , Detail, BikePicLink )
+VALUES ( 1, 1, False, 'Nogizaka46Bicycle', 'https://drive.google.com/thumbnail?id=1y28vUTqGdBcRcG6XOgb5qkw92HiIMblq');
+
+
+INSERT INTO Bicycle ( BikeID , LocationID ,IsAvailable , Detail, BikePicLink )
+VALUES ( 2, 2, False, 'Nogizaka46Bicycle', 'https://drive.google.com/thumbnail?id=1lA6pln9eE6huinZ2EBxl3P0H_J9Qt5OP');
+
+
+INSERT INTO Bicycle ( BikeID , LocationID ,IsAvailable , Detail, BikePicLink )
+VALUES ( 3, 3, False, 'Nogizaka46Bicycle', 'https://drive.google.com/thumbnail?id=1ieHC54zUnRo4PwYeV27hYBJO25-SJ4ha');
+
+
+INSERT INTO Bicycle ( BikeID , LocationID ,IsAvailable , Detail, BikePicLink )
+VALUES ( 4, 4, False, 'Nogizaka46Bicycle', 'https://drive.google.com/thumbnail?id=1QhCiT3gWMB_g133YcAsG6Hp1TP-yDCnY');
+
+
+INSERT INTO Bicycle ( BikeID , LocationID ,IsAvailable , Detail, BikePicLink )
+VALUES ( 5, 5, False, 'Nogizaka46Bicycle', 'https://drive.google.com/thumbnail?id=1m_G4iIpaPSTSWvCypzEoF5gw0bJqXpE8');
+
+
+INSERT INTO Bicycle ( BikeID , LocationID ,IsAvailable , Detail, BikePicLink )
+VALUES ( 6, 6, False, 'Nogizaka46Bicycle', 'https://drive.google.com/thumbnail?id=1BvdQjYMW0bOVsxMwlMrSzn4dTYmjet0a');
+
+
+INSERT INTO Bicycle ( BikeID , LocationID ,IsAvailable , Detail, BikePicLink )
+VALUES ( 7, 7, False, 'Nogizaka46Bicycle', 'https://drive.google.com/thumbnail?id=1u7eLEQ5ZpetdK1ktgBD6b2g6dZkokF7R');
+
+
+INSERT INTO Bicycle ( BikeID , LocationID ,IsAvailable , Detail, BikePicLink )
+VALUES ( 8, 8, True, 'Nogizaka46Bicycle', 'https://drive.google.com/thumbnail?id=1tmyb5FJ9c2bEhjWBUHGCQxRuTplhXBDG');
 
 
 -- ___________________________End_Bicycle____________________________
-
-
-
-
-
-
-
 
 
 
