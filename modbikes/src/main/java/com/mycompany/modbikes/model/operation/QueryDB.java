5f48760ce1bd91329765dbb62fb80459d11a2f83
Nuntuch <nuntuch13589@gmail.com>
2021-04-21 17:16:41 +0700

    สร้าง function ตรวจสอบการมีอยู่ของ user password ที่กรอกมาใน DB

--- modbikes/src/main/java/com/mycompany/modbikes/model/operation/QueryDB.java
+++ modbikes/src/main/java/com/mycompany/modbikes/model/operation/QueryDB.java
@@ -22,10 +22,14 @@ import java.util.logging.Logger;
 public class QueryDB {
 
     public static void main(String args[]) {
-        SelectStarFromStudent();
-        SelectStarFromLocation();
-        SelectStarFromHistory();
-        SelectStarFromBicycle();
+//        SelectStarFromStudent();
+//        SelectStarFromLocation();
+//        SelectStarFromHistory();
+//        SelectStarFromBicycle();
+
+        System.out.println(CheckUsernameAndPassword("Akimoto.Manatsu@gmail.com", "12646"));
+        System.out.println(CheckUsernameAndPassword("Akimoto.Manatsu@gmail.com", "04446"));
+        System.out.println(CheckUsernameAndPassword("ikoma.rina@gmail.com", "04446"));
     }
 
     public static void SelectStarFromStudent() {
@@ -70,6 +74,8 @@ public class QueryDB {
                 System.out.println("Email = " + Email);
                 System.out.println("OtherContact = " + OtherContact);
                 System.out.println();
+                
+                
             }
             rs.close();
             stmt.close();
@@ -253,4 +259,72 @@ public class QueryDB {
 
     }
 
+    
+//    __________________________________________________________________________
+    
+        public static boolean CheckUsernameAndPassword(String user_email, String user_password) {
+        boolean is_have_user = false;
+//             url = jdbc:postgresql://<host>:<port>/<dbname>?user=<username>&password=<password>
+        String url = "jdbc:postgresql://ec2-3-234-85-177.compute-1.amazonaws.com:5432/der31l0fqg9f6b?user=nkqwwjhlpbldun&password=fd0a974ab729bef38965fe05a1dd9132d0b07dba3e34d598a540120c2cd84231";
+        String user = "nkqwwjhlpbldun";
+        String password = "fd0a974ab729bef38965fe05a1dd9132d0b07dba3e34d598a540120c2cd84231";
+
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection(url,
+                            user, password);
+            c.setAutoCommit(false);
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            
+            String query_stagement = "SELECT Email,Password FROM Student WHERE Email = '"+user_email+"' AND Password = '"+user_password +"';";
+            
+            System.out.println("query_stagement : " + query_stagement);
+            
+            ResultSet rs = stmt.executeQuery(query_stagement);
+            while (rs.next()) {
+              
+                String Email = rs.getString("Email");
+                String Password = rs.getString("Password");
+              
+              
+                System.out.println("Email = " + Email);
+                System.out.println("Password = " + Password);
+               
+                System.out.println();
+                
+                is_have_user = true;
+
+            }
+            rs.close();
+            stmt.close();
+            c.close();
+//            is_have_user = true;
+            
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+//            System.exit(0);
+            is_have_user = false;
+        }
+        System.out.println("Operation done successfully");
+
+        try {
+            c.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        System.out.println("Close database successfully");
+
+        
+        return is_have_user;
+    }
+    
+       
+    
+    
 }
