81a86badf0d1bbdeb2f64d52a54cdfe82be29598
Nuntuch <nuntuch13589@gmail.com>
2021-04-27 23:16:34 +0700

    แก้ฟังก์ชั่น Login ให้ทำงานโดยเช็คกับ DB จริงๆ

--- modbikes/src/main/java/com/mycompany/modbikes/servlet/LoginServlet.java
+++ modbikes/src/main/java/com/mycompany/modbikes/servlet/LoginServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import com.mycompany.modbikes.model.operation.QueryDB;
 /**
  *
  * @author Admin
@@ -36,32 +36,35 @@ public class LoginServlet extends HttpServlet {
         String idS = request.getParameter("id");
         String pinS = request.getParameter("pin");
 
-//        if (idS != null && idS.trim().length() > 0
-//                && pinS != null && pinS.trim().length() > 0) {
-//
-//            int id = Integer.parseInt(idS);
-//            int pin = Integer.parseInt(pinS);
-//
-//            AccountJpaController accountJpaCtrl = new AccountJpaController(utx, emf);
-//// //            Account accountObj = accountJpaCtrl.findAccount(id);
-//            if (accountObj != null) {
-//
-//                if (accountObj.getPin() == pin) {
-//                    HttpSession session = request.getSession(true);
-//                    session.setAttribute("account", accountObj);
-////                    getServletContext().getRequestDispatcher("/MyAccount").forward(request, response);
-//                    response.sendRedirect(getServletContext().getContextPath()+"/MyAccount");
-//                    return;
-//                } else {
-//                    request.setAttribute("status", "PIN Worng!!!");
-//                    getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
-//                    return;
-//                }
-//
-//            }
-//            request.setAttribute("status", "ID Worng!!!");
-//            getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
-//        }
+        if (idS != null && idS.trim().length() > 0
+                && pinS != null && pinS.trim().length() > 0) {
+
+            int id = Integer.parseInt(idS);
+            int pin = Integer.parseInt(pinS);
+
+            QueryDB queryDBObj = new QueryDB();
+            boolean canlogin = queryDBObj.CheckUsernameAndPassword(pinS, pinS)
+            
+            AccountJpaController accountJpaCtrl = new AccountJpaController(utx, emf);
+// //            Account accountObj = accountJpaCtrl.findAccount(id);
+            if (accountObj != null) {
+
+                if (accountObj.getPin() == pin) {
+                    HttpSession session = request.getSession(true);
+                    session.setAttribute("account", accountObj);
+//                    getServletContext().getRequestDispatcher("/MyAccount").forward(request, response);
+                    response.sendRedirect(getServletContext().getContextPath()+"/MyAccount");
+                    return;
+                } else {
+                    request.setAttribute("status", "PIN Worng!!!");
+                    getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+                    return;
+                }
+
+            }
+            request.setAttribute("status", "ID Worng!!!");
+            getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+        }
         getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
 
 
